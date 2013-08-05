88d437194bfd4619e440ae98a0a225ba885ff55c
klm21904 <ramon.makkelie@klm.com>
2013-08-05 13:27:51 +0200

    added system variables maven

---
+++ recipes/agent.rb
@@ -0,0 +1,27 @@
+
+
+remote_file "/opt/atlassian-bamboo-agent-installer.jar" do
+  source "#{node['bamboo']['url']}/agentServer/agentInstaller/atlassian-bamboo-agent-installer-#{node['bamboo']['version']}.jar"
+  mode "0644"
+  not_if { ::File.exists?("/opt/bamboo/agent/atlassian-bamboo-agent-installer.jar") }
+end
+
+execute "java -Dbamboo.home=/opt/bamboo -jar opt/bamboo/agent/atlassian-bamboo-agent-installer.jar #{node['bamboo']['url']}/agentServer/ install" do
+  not_if { ::File.exists?("/opt/bamboo/agent/.installed") }
+end
+
+file "/opt/bamboo/agent/.installed" do
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create_if_missing
+end
+
+link "/etc/init.d/bamboo-agent" do
+  to "/opt/bamboo/bin/bamboo-agent.sh"
+end
+
+service "bamboo-agent" do
+  supports :restart => true, :status => true, :start => true, :stop => true
+  action [:enable, :start]
+end
