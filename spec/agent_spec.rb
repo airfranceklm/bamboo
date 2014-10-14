d2ce2e28f9f51cadd98f6bb69edc02b120b5507a
Nicolas Blanc <sinfomicien@gmail.com>
2014-10-14 11:31:08 +0200

    Workaround ChefSpec strange execution plan for lwrp

--- spec/agent_spec.rb
+++ spec/agent_spec.rb
@@ -9,6 +9,7 @@ describe 'debian::bamboo::agent' do
                                  ) do |node|
       node.automatic['memory']['total'] = '2048kB'
       node.automatic['ipaddress'] = '1.1.1.1'
+      node.set['bamboo']['agent_capabilities']['chefspec'] = '/here/i/am'
     end
     runner.converge('bamboo::_unit_testing_agent_capability')
   end
@@ -16,6 +17,8 @@ describe 'debian::bamboo::agent' do
   it 'template[bamboo-capabilities.properties]' do
     expect(chef_run).to create_template('bamboo-capabilities.properties')
     expect(chef_run).to render_file('bamboo-capabilities.properties')
-      .with_content(/system.of.a.down/)
+      .with_content(/chefspec/)
+    expect(chef_run.node['bamboo']['agent_capabilities']['system.git.executable']).to eq ('/usr/local/bin/git')
+    expect(chef_run.node['bamboo']['agent_capabilities']['system.of.a.down']).to eq ('Toxicity')
   end
 end
