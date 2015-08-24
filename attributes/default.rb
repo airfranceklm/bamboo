42dc1e582e781515f5d67be0d1089768420b4754
ojawel <j.tomasoa@gmail.com>
2015-08-24 11:12:32 +0200

    set postgress tune and shasum for 5.9.4

--- attributes/default.rb
+++ attributes/default.rb
@@ -38,6 +38,7 @@ when '5.6.2' then '57737a00207642e7be4ceea7b702e30e1caccaaa51e08699c2c9ae588e4c8
 when '5.7.2' then 'b378500ea61803333fc27b0a42cfbdfbce5dccc75cead0b464dbc4e5e0bddb17'
 when '5.8.1' then 'bb691d22273ceaa999eb435dfb4e8d697c3c803505218845bf5785ec2785cbd8'
 when '5.9.1' then '0b7e84214862c4afd7dbc839c2c4045d28e554c383d93660b6d2e0bce42898d3'
+when '5.9.4' then '8905a4750ca6d73feefbcc58d91e05be3bd60e2c60ca422f092a080f7263bdb2'
 end
 
 default[:bamboo][:database][:external]            = false
@@ -51,6 +52,7 @@ when 'postgresql'
   default[:postgresql][:version]                  = '9.4'
   default[:bamboo][:database][:host]              = 'localhost'
   default[:bamboo][:database][:port]              = 5432
+  default[:postgresql][:config_pgtune][:db_type]  = 'web'
 end
 default[:bamboo][:database][:name]                = 'bamboo'
 default[:bamboo][:database][:user]                = 'bamboo'
